bb5a6465fa59be6179038e01ff49a62b83ab28a9
Niels <77604434+nielsmai@users.noreply.github.com>
2025-03-18 19:54:25 -0400

    Updated favicons

--- acnh_chat/app/layout.tsx
+++ acnh_chat/app/layout.tsx
@@ -29,6 +29,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
